cc6500ab6d8286696053493901cb93a87c48d82e
ThabangLigavha710 <57515903+ThabangLigavha710@users.noreply.github.com>
2024-09-01 17:50:45 +0200

    Uploading Files

---
+++ application.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/data")
+def data():
+    return {"data": "example"}
+
+@app.route("/data", methods=["POST"])
+def extract():
+
+    output = []
+
+    extract_data = data()['data']
+
+    for i in extract_data:
+        output.append(i)
+
+    output.sort()
+
+    extracted_data = {"word": output}
+    return extracted_data
+
+extract()
